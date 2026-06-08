cfa4d94f3a445872da2a7c1102d503cf33a2bf5f
mareddysrichandanareddy <mareddysrichandanareddy@gmail.com>
2026-06-08 17:03:15 +0530

    Update planner.py

--- planner.py
+++ planner.py
@@ -1,1 +1,15 @@
-print("Planner Module")
+def generate_schedule(subject, hours):
+    try:
+        hours = int(hours)
+
+        schedule = f"\nStudy Schedule\n"
+        schedule += f"\nSubject: {subject}\n"
+        schedule += f"Study Time: {hours} Hours\n"
+
+        for i in range(1, hours + 1):
+            schedule += f"Hour {i}: Study {subject}\n"
+
+        return schedule
+
+    except:
+        return "Please enter valid study hours."
